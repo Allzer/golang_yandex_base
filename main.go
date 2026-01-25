667e09b20e38ae92f41c1e8c7ad64c6273d54a97
Allzer <dshapkarin@list.ru@>
2026-01-26 00:40:31 +0300

    базовые указатели

--- main.go
+++ main.go
@@ -1,25 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	array := []int{}
 
-	enterTransactions(&array)
-	fmt.Print(array)
+	a := 2
+	b := 3
+
+	var res int
+	
+	quatro(&a, &b, &res)
+
+	fmt.Println(res)
+
 }
 
-func enterTransactions(array *[]int) {
-	transaction := 0
-	for {
-		fmt.Scan(&transaction)
-		if transaction == -10 || transaction == 10 || transaction == 40 {
-			*array = append(*array, transaction)
-		}
-		if len(*array) == 3{
-			break
-		}
-	} 
+func quatro(num_a, num_b, res *int) {
+	*res = *num_a + *num_b
 }
